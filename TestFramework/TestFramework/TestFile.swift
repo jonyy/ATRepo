a0baa3bd8c8ef9c24a53f6e03b9c0dff7a8cd8b5
ArshadT <arshad.tamboli@extentia.com>
2023-07-11 15:42:33 +0530

    - Initial Commit

---
+++ TestFramework/TestFramework/TestFile.swift
@@ -0,0 +1,17 @@
+//
+//  TestFile.swift
+//  TestFramework
+//
+//  Created by Arshad Tamboli on 7/11/23.
+//
+
+import Foundation
+
+
+class TestClass {
+    
+    func testFunction() -> String {
+        return "Hello AT"
+    }
+    
+}
